189152fcb0f114225c24a6299867e080b749e347
tuantran1986 <61728265+tuantran1986@users.noreply.github.com>
2024-08-17 22:43:32 +0700

    bai 3 - router

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 
-export default function Home() {
+export default function AboutPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      <div>HOME-PAGE</div>
+      <div>ABOUT-PAGE</div>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis ex libero excepturi nam qui eveniet totam molestiae numquam pariatur iste commodi vitae, delectus, sapiente rem eaque reprehenderit, a optio animi!</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis ex libero excepturi nam qui eveniet totam molestiae numquam pariatur iste commodi vitae, delectus, sapiente rem eaque reprehenderit, a optio animi!</p>
 
